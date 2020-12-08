8e13235a3d037f884c66cb85e4601303b83534c5
Emily <sakuya0116@hotmail.com>
2020-12-07 22:38:05 -0800

    no message

---
+++ Portfolio/contact.php
@@ -0,0 +1,18 @@
+<?php 
+    $name = $_POST['name'];
+
+    $email = $_POST['email'];
+
+    $subject = $_POST['subject'];
+
+    $message = $_POST['message'];
+
+    if (($name!="")||($email!="")||($message!="")) {   
+
+        $from = "From: $name<$email>\r\nReturn-path: $email";
+
+        // Change to your email
+        mail("YOUR MAIL", $subject, $message, $from);  
+
+    }    
+?> 
